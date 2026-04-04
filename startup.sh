27a8646a6fb2bd054cb64bc437bd922b73d274d9
Satya-Sai-Srinivas <satyasaisrinivas955@gmail.com>
2026-04-04 16:30:15 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
+
 echo "startup.sh running"
-ls -la
-echo "Starting gunicorn now"
-exec gunicorn --bind=0.0.0.0:${PORT:-8000} --timeout 600 app:app
+echo "Starting gunicorn..."
+
+exec gunicorn --bind 0.0.0.0:${PORT:-8000} --timeout 600 app:app
